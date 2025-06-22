0ddb9783b27a88a2e3f1b4e328e1e5f2212411ce
Arachne <66822642+Arachneee@users.noreply.github.com>
2025-06-22 16:17:56 +0900

    feat: add index direction

--- src/main/kotlin/com/quantai/domain/StockCode.kt
+++ src/main/kotlin/com/quantai/domain/StockCode.kt
@@ -2,6 +2,7 @@ package com.quantai.domain
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.index.CompoundIndex
+import org.springframework.data.mongodb.core.index.IndexDirection
 import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDateTime
@@ -11,7 +12,7 @@ import java.time.LocalDateTime
  * 두 종류의 종목 데이터를 모두 수용할 수 있는 유연한 구조
  */
 @Document(collection = "stock_codes")
-@CompoundIndex(name = "marketCap_code_idx", def = "{'marketCap': 1, 'code': 1}")
+@CompoundIndex(name = "marketCap_code_idx", def = "{'marketCap': -1, 'code': 1}")
 data class StockCode(
     @Id
     val id: String? = null,
@@ -25,7 +26,7 @@ data class StockCode(
 
     val standardCode: String? = null,
 
-    @Indexed
+    @Indexed(direction = IndexDirection.DESCENDING)
     val marketCap: Int? = null,
 
     val additionalAttributes: MutableMap<String, String> = mutableMapOf(),
