c2b895996a3e8a5ffd8414dac6f4130bd5c7cadd
Arachne <66822642+Arachneee@users.noreply.github.com>
2025-06-22 14:24:48 +0900

    refactor: delete query annotation

--- src/main/kotlin/com/quantai/domain/StockCodeRepository.kt
+++ src/main/kotlin/com/quantai/domain/StockCodeRepository.kt
@@ -1,7 +1,6 @@
 package com.quantai.domain
 
 import org.springframework.data.domain.Pageable
-import org.springframework.data.mongodb.repository.Query
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
@@ -9,7 +8,6 @@ import reactor.core.publisher.Flux
 @Repository
 interface StockCodeRepository : ReactiveMongoRepository<StockCode, String> {
 
-    @Query(value = "{ 'marketCap': { \$ne: null } }", sort = "{ 'marketCap': -1 }")
     fun findByMarketCapNotNullOrderByMarketCapDesc(pageable: Pageable): Flux<StockCode>
 }
 
